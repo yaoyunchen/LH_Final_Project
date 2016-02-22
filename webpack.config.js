13d268376b1b2c902a5fddd7220fbb00d0b98d97
Elaya Sison <asison13@gmail.com>
2016-02-22 14:29:20 -0800

    Added Node and React files

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  entry: './js/main.js',
+  output: {
+    path: './',
+    filename: 'index.js'
+  },
+  devServer: {
+   // Lets the app reload on the fly.
+   inline: true,
+   // Sets the host.
+   host: '0.0.0.0',
+   // Specifies the port.
+   port: 3000,
+   watchOptions: {
+     aggregateTimeout: 300,
+     poll: 1000
+   }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+}
