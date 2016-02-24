60ad3fdd7dc50e07db87fc44ebb4149e5071ccce
Andy Yao <a_n_d_y_y_a_o@hotmail.com>
2016-02-24 10:49:52 -0800

    Fixed random video selection when over 200 pages.

--- js/App.js
+++ js/App.js
@@ -14,7 +14,7 @@ var App = {
       getCode: false,
       countryCache: '',
       cityName: '',
-      playMode: 'videos'
+      playMode: 'videos',
     }
   },
 
@@ -26,16 +26,12 @@ var App = {
     }, function() {
       this.flickrFindPlace(this.props.countryCode)
     });
-    console.log("HANDLING MAP CLICK");
-    console.log('first', this);
     
   },
 
 
   flickrFindPlace : function(code) {
     var that = this
-    console.log("FINDING PLACE")
-    console.log('second', this)
 
     var strUrl = "https://api.flickr.com/services/rest/?&method=flickr.places.find&api_key=c01e0fde2a3823f1e80eed24d5b80e63&query=" + this.state.countryCode + "&format=json&nojsoncallback=1"
     
@@ -43,24 +39,52 @@ var App = {
       for (var i=0; i < results.places.place.length; i ++) {
           if (results.places.place[i].place_type == 'country') {
             var country = results.places.place[i];
-            that.flickrPhotoSearch(country.place_id, country.woeid);
+            that.flickrPhotoPage(country.place_id, country.woeid);
           }
       }
     })
   },
 
-  flickrPhotoSearch : function(place_id, woe_id){
+  flickrPhotoPage : function(place_id, woe_id){
     var that = this;
-    var strUrl = "https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=c01e0fde2a3823f1e80eed24d5b80e63&woe_id=" + woe_id + "&place_id=" + place_id + "&media=videos&per_page=10&format=json&nojsoncallback=1"
-    
-    // Add &pages to randomize played videos
+    var strUrl = "https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=c01e0fde2a3823f1e80eed24d5b80e63&woe_id=" + woe_id + "&place_id=" + place_id + "&media=videos&per_page=10&page=&format=json&nojsoncallback=1";
 
     this.serverRequest = $.get(strUrl, function(results){
-      var photo = results.photos.photo[0];
-      
-      // for (var i=0; i < photos.length; i ++) {
-      //     //console.log(results.photos.photos[i])
-      // }
+
+      var pagesNumber = results.photos.pages;
+      var totalVideos = 10
+
+      if (pagesNumber == 1) {
+        totalVideos = results.photos.total;
+      } else {
+        if (pagesNumber > 200) {
+          pagesNumber = 200;
+        }
+        pagesNumber = Math.floor(Math.random() * (pagesNumber - 0 + 1) + 0);  
+      }
+
+      var arrayLength = results.photos.photo.length;
+      while (arrayLength == 0) {
+        pagesNumber = 1;
+      } 
+
+      that.flickrPhotoSearch(place_id,woe_id, totalVideos, pagesNumber)
+    })
+
+  },
+
+  flickrPhotoSearch : function(place_id, woe_id, totalVideos, pagesNumber){
+    var that = this;
+    var strUrl = "https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=c01e0fde2a3823f1e80eed24d5b80e63&woe_id=" + woe_id + "&place_id=" + place_id + "&media=videos&per_page=" +totalVideos + "&page=" + pagesNumber + "&format=json&nojsoncallback=1"
+    
+    // Add &pages to randomize played videos
+    console.log('Photo search', strUrl)
+    this.serverRequest = $.get(strUrl, function(results){
+
+      var randomVideo = Math.floor(Math.random() * (totalVideos));
+
+      var photo = results.photos.photo[randomVideo];
+
       that.flickrGetSizes(photo.id, photo.owner, photo.title);
       
     })
@@ -70,7 +94,7 @@ var App = {
   flickrGetSizes : function(id, owner, title) {
     var that = this
     var strUrl = "https://api.flickr.com/services/rest/?&method=flickr.photos.getSizes&api_key=c01e0fde2a3823f1e80eed24d5b80e63&photo_id=" + id + "&format=json&nojsoncallback=1"
-
+    console.log('Get sizes', strUrl)
     this.serverRequest = $.get(strUrl, function(results){
       
       var size = results.sizes.size[results.sizes.size.length - 1];
@@ -110,6 +134,3 @@ var App = {
 };
 
 export default React.createClass(App)
-
-// call and bind
-
