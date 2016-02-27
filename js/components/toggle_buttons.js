fb3a91d78789f36353ed45f3cc10de719a45e82e
Kenny Wu <wu.kenny1221@gmail.com>
2016-02-27 14:35:01 -0800

    fix toggle buttons style and onClick state

--- js/components/toggle_buttons.js
+++ js/components/toggle_buttons.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
 var ToggleButtons = {
+  getInitialState(){
+    return {
+      eyeStatus: 'focused',
+      musicStatus: ''
+    }
+  },
+
+  handleEyeClick(){
+    this.setState({
+      eyeStatus: 'focused',
+      musicStatus: ''
+    })
+  },
+
+  handleMusicClick(){
+    this.setState({
+      eyeStatus: '',
+      musicStatus: 'focused'
+    })  
+  },
+
   render() {
     var eye_content = <i className="material-icons">visibility</i>
     var audio_content = <i className="material-icons">headset</i>
     return (
       <div id="mode-toggle">
-        <button id="video-toggle">{eye_content}</button>
-        <button id="slideshow-toggle">{audio_content}</button>
+        <button onClick={this.handleEyeClick} className={this.state.eyeStatus} id="video-toggle">{eye_content}</button>
+        <button onClick={this.handleMusicClick} className={this.state.musicStatus} id="slideshow-toggle">{audio_content}</button>
       </div>
     )
   }
