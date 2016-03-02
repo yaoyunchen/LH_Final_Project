558b32c1ae1564dba47ab377792d4d54084f6e1c
Kenny Wu <wu.kenny1221@gmail.com>
2016-03-01 16:03:24 -0800

    initial deploy

--- app/actions.rb
+++
@@ -1,9 +0,0 @@
-# Homepage (Root path)
-
-configure do
-  set :views, Proc.new { root }
-end
-
-get '/' do
-  erb :'index.html'
-end
--- db/seeds.rb
+++
@@ -1,8 +0,0 @@
-1000.times do
-  title = Faker::Lorem.word
-  Page.create(
-    title: title.titleize,
-    slug: title,
-    content: Faker::Lorem.paragraphs(5).join("\n\n")
-  )
-end
